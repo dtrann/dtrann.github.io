35c91141523aef487b7427a7169f079245a26693
Damon Tran <damontran_@hotmail.com>
2016-09-20 19:37:21 -0600

    added links, still need to fix scrolling

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
-   $("#back-to-top").click(function(){
+    $("#back-to-top").click(function(){
         scroll(0,0);
-   });
+    });
+    
+    $("#about-me").click(function() {
+        $("#about").get(0).scrollIntoView();
+    });
+    
+    $("#my-work").click(function(){
+        $("#portfolio").get(0).scrollIntoView();
+    });
+    
+    $("#contact-me").click(function(){
+        $("footer").get(0).scrollIntoView();
+    });
 });
